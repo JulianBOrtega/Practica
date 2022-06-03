7e10931a3334440ce5995d013ffe19a0fe3e5b23
ushiovii@hotmail.com <ushiovii@hotmail.com>
2022-06-03 17:59:20 -0300

    app.js, armas.json & enemigos.json | Actualizados

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const armas = JSON.parse(fs.readFileSync("./armas.json", "utf-8"));
 const enemigos = JSON.parse(fs.readFileSync("./enemigos.json", "utf-8"));
 
 nivel.generarNivel();
-nivel.mapa.editarCelda([0, 2], "*");
+nivel.mapa.editarCelda([0, 2], "♚");
 nivel.agregarEnemigo(enemigos[0], [5, 2]);
 
 console.log(nivel.mapa.celdas);
