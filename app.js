2ecd8a8f899a68dd0f86bd2a4f0a99a478375f83
Julián <j.gakusei11@gmail.com>
2022-06-03 03:13:53 -0300

    app.js actualizado. agregarEnemigo() añadido a manager.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 console.clear();
+const fs = require("fs");
 const nivel = require("./manager.js");
+const armas = JSON.parse(fs.readFileSync("./armas.json", "utf-8"));
+const enemigos = JSON.parse(fs.readFileSync("./enemigos.json", "utf-8"));
 
 nivel.generarNivel();
 nivel.mapa.editarCelda([0, 2], "*");
+nivel.agregarEnemigo(enemigos[0], [5, 2]);
 
 console.log(nivel.mapa.celdas);
--- manager.js
+++ manager.js
@@ -6,7 +6,12 @@ const nivel = {
     {
         return this.mapa.generarCeldas();
     },
-
+    agregarEnemigo: function(enemigo, posicion)
+    {
+        this.mapa.editarCelda(posicion, enemigo.icono);
+        enemigo.posicion = posicion;
+        this.enemigos.push(enemigo);
+    }
 }
 
 module.exports = nivel;
